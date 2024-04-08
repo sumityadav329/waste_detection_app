ff4ecba07163e68c5db9bbad0a0c4db8005af275
Sumit Yadav <sumityadav329@gmail.com>
2024-04-08 19:33:04 +0530

    data validation added

--- wasteDetection/pipeline/training_pipeline.py
+++ wasteDetection/pipeline/training_pipeline.py
@@ -2,7 +2,7 @@ import sys, os
 from wasteDetection.logger import logging
 from wasteDetection.exception import AppException
 from wasteDetection.components.data_ingestion import DataIngestion
-##from wasteDetection.components.data_validation import DataValidation
+from wasteDetection.components.data_validation import DataValidation
 #from wasteDetection.components.model_trainer import ModelTrainer
 
 
@@ -18,7 +18,7 @@ from wasteDetection.entity.artifacts_entity import (DataIngestionArtifact,
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
-        #self.data_validation_config = DataValidationConfig()
+        self.data_validation_config = DataValidationConfig()
         #self.model_trainer_config = ModelTrainerConfig()
 
 
@@ -45,20 +45,44 @@ class TrainPipeline:
         except Exception as e:
             raise AppException(e, sys)
         
+    def start_data_validation(
+        self, data_ingestion_artifact: DataIngestionArtifact
+    ) -> DataValidationArtifact:
+        logging.info("Entered the start_data_validation method of TrainPipeline class")
+
+        try:
+            data_validation = DataValidation(
+                data_ingestion_artifact=data_ingestion_artifact,
+                data_validation_config=self.data_validation_config,
+            )
+
+            data_validation_artifact = data_validation.initiate_data_validation()
+
+            logging.info("Performed the data validation operation")
+
+            logging.info(
+                "Exited the start_data_validation method of TrainPipeline class"
+            )
+
+            return data_validation_artifact
+
+        except Exception as e:
+            raise AppException(e, sys) from e
+        
 
     def run_pipeline(self) -> None:
         try:
             data_ingestion_artifact = self.start_data_ingestion()
-            '''data_validation_artifact = self.start_data_validation(
+            data_validation_artifact = self.start_data_validation(
                 data_ingestion_artifact=data_ingestion_artifact
             )
-
+            """
             if data_validation_artifact.validation_status == True:
                 model_trainer_artifact = self.start_model_trainer()
             
             else:
-                raise Exception("Your data is not in correct format")
-'''
+                raise Exception("Your data is not in correct format")"""
+            
         
         except Exception as e:
             raise AppException(e, sys)
